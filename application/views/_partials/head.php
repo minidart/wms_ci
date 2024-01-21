bffaea30387c8d4574a08de1a1dff9fb279f92dc
minidart <opangay@gmail.com>
2024-01-21 19:50:54 +0700

    create home page

---
+++ application/views/_partials/head.php
@@ -0,0 +1,33 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?= $title ?></title>
+
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+<!-- Font Awesome -->
+<link rel="stylesheet" href="<?= base_url('assets/css/library/fontawesome-free/css/all.min.css') ?>">
+
+<!-- Ionicons -->
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+<!-- Tempusdominus Bootstrap 4 -->
+<link rel="stylesheet" href="<?= base_url('assets/css/library/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') ?>">
+
+<!-- iCheck -->
+<link rel="stylesheet" href="<?= base_url('assets/css/library/icheck-bootstrap/icheck-bootstrap.min.css') ?>">
+
+<!-- JQVMap -->
+<link rel="stylesheet" href="<?= base_url('assets/css/library/jqvmap/jqvmap.min.css') ?>">
+
+<!-- Theme style -->
+<link rel="stylesheet" href="<?= base_url('assets/css/library/adminlte/adminlte.min.css') ?>">
+
+<!-- overlayScrollbars -->
+<link rel="stylesheet" href="<?= base_url('assets/css/library/overlayScrollbars/css/OverlayScrollbars.min.css') ?>">
+
+<!-- Daterange picker -->
+<link rel="stylesheet" href="<?= base_url('assets/css/library/daterangepicker/daterangepicker.css') ?>">
+
+<!-- summernote -->
+<link rel="stylesheet" href="<?= base_url('assets/css/library/summernote/summernote-bs4.min.css') ?>">
